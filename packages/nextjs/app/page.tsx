346e43668d638ea6b7818ab3a222435559d20d5d
juan <jpredcoin@gmail.com>
2024-06-25 22:34:44 -0500

    probando componentes daisy UI, sigue hacer la imagen del hero

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -21,6 +21,216 @@ const Home: NextPage = () => {
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
+          <div
+            className="hero min-h-screen"
+            style={{
+              backgroundImage: "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+            }}
+          >
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-neutral-content text-center">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                <p className="mb-5">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+                  deleniti eaque aut repudiandae et a id nisi.
+                </p>
+                <button className="btn btn-primary">Get Started</button>
+              </div>
+            </div>
+          </div>
+          <div className="carousel rounded-box">
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+            </div>
+          </div>
+
+          <div className="carousel rounded-box w-64">
+            <div className="carousel-item w-full">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+                className="w-full"
+                alt="Tailwind CSS Carousel component"
+              />
+            </div>
+            <div className="carousel-item w-full">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+                className="w-full"
+                alt="Tailwind CSS Carousel component"
+              />
+            </div>
+            <div className="carousel-item w-full">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+                className="w-full"
+                alt="Tailwind CSS Carousel component"
+              />
+            </div>
+            <div className="carousel-item w-full">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+                className="w-full"
+                alt="Tailwind CSS Carousel component"
+              />
+            </div>
+            <div className="carousel-item w-full">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+                className="w-full"
+                alt="Tailwind CSS Carousel component"
+              />
+            </div>
+            <div className="carousel-item w-full">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+                className="w-full"
+                alt="Tailwind CSS Carousel component"
+              />
+            </div>
+            <div className="carousel-item w-full">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+                className="w-full"
+                alt="Tailwind CSS Carousel component"
+              />
+            </div>
+          </div>
+
+          <div className="carousel w-full">
+            <div id="item1" className="carousel-item w-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+            </div>
+            <div id="item2" className="carousel-item w-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+            </div>
+            <div id="item3" className="carousel-item w-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+            </div>
+            <div id="item4" className="carousel-item w-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+            </div>
+          </div>
+          <div className="flex w-full justify-center gap-2 py-2">
+            <a href="#item1" className="btn btn-xs">
+              1
+            </a>
+            <a href="#item2" className="btn btn-xs">
+              2
+            </a>
+            <a href="#item3" className="btn btn-xs">
+              3
+            </a>
+            <a href="#item4" className="btn btn-xs">
+              4
+            </a>
+          </div>
+          <ul className="timeline timeline-vertical lg:timeline-horizontal">
+            <li>
+              <div className="timeline-start">1984</div>
+              <div className="timeline-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end timeline-box">First Macintosh computer</div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-start">1998</div>
+              <div className="timeline-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end timeline-box">iMac</div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-start">2001</div>
+              <div className="timeline-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end timeline-box">iPod</div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-start">2007</div>
+              <div className="timeline-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end timeline-box">iPhone</div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-start">2015</div>
+              <div className="timeline-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end timeline-box">Apple Watch</div>
+            </li>
+          </ul>
+          <div className="skeleton h-32 w-32"></div>
+          <div className="flex w-full flex-col">
+            <div className="divider">Default</div>
+            <div className="divider divider-neutral">Neutral</div>
+            <div className="divider divider-primary">Primary</div>
+            <div className="divider divider-secondary">Secondary</div>
+            <div className="divider divider-accent">Accent</div>
+            <div className="divider divider-success">Success</div>
+            <div className="divider divider-warning">Warning</div>
+            <div className="divider divider-info">Info</div>
+            <div className="divider divider-error">Error</div>
+          </div>
+
           {/* <p className="text-center text-lg">
             Get started by editing{" "}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,32 +1,47 @@
 import React from "react";
 //import Link from "next/link";
-import { hardhat } from "viem/chains";
+//import { hardhat } from "viem/chains";
 //import { CurrencyDollarIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 //import { HeartIcon } from "@heroicons/react/24/outline";
-import { SwitchTheme } from "~~/components/SwitchTheme";
+//import { SwitchTheme } from "~~/components/SwitchTheme";
 import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
+
 //import { Faucet } from "~~/components/scaffold-eth";
-import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
-import { useGlobalState } from "~~/services/store/store";
+//import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
+//import { useGlobalState } from "~~/services/store/store";
 
 /**
  * Site footer
  */
 export const Footer = () => {
-  const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrency.price);
-  const { targetNetwork } = useTargetNetwork();
-  const isLocalNetwork = targetNetwork.id === hardhat.id;
+  // const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrency.price);
+  // const { targetNetwork } = useTargetNetwork();
+  // const isLocalNetwork = targetNetwork.id === hardhat.id;
 
   return (
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
       <div>
         <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
-          <div className="flex flex-col md:flex-row gap-2 pointer-events-auto">
+          {/* <div className="flex flex-col md:flex-row gap-2 pointer-events-auto">
             {nativeCurrencyPrice > 0 && <div></div>}
-          </div>
-          <SwitchTheme className={`pointer-events-auto ${isLocalNetwork ? "self-end md:self-auto" : ""}`} />
+          </div> */}
+          {/* <SwitchTheme className={`pointer-events-auto ${isLocalNetwork ? "self-end md:self-auto" : ""}`} /> */}
         </div>
       </div>
+      <footer className="footer bg-base-200 text-base-content p-10">
+        <form>
+          <h6 className="footer-title">Newsletter</h6>
+          <fieldset className="form-control w-80">
+            <label className="label">
+              <span className="label-text">Enter your email address</span>
+            </label>
+            <div className="join">
+              <input type="text" placeholder="username@site.com" className="input input-bordered join-item" />
+              <button className="btn btn-primary join-item">Subscribe</button>
+            </div>
+          </fieldset>
+        </form>
+      </footer>
       <div className="w-full">
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">

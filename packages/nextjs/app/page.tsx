28583f7718ca5d47791bbe69a11be8323b937574
juan <jpredcoin@gmail.com>
2024-06-26 13:39:29 -0500

    listo el hero y aboutme, sigue hard y soft skills

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -12,11 +12,7 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          {/* <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1> */}
+        <div className="px-5 w-full">
           <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
@@ -25,9 +21,9 @@ const Home: NextPage = () => {
             className="hero min-h-screen"
             style={{
               backgroundImage: "url(/Design1.png)",
-              backgroundSize: "contain", // Ajusta la imagen para cubrir todo el contenedor
-              backgroundPosition: "center", // Centra la imagen
-              backgroundRepeat: "no-repeat", // Evita que la imagen se repita
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
             }}
           >
             <div className="hero-content text-neutral-content text-center flex flex-col justify-end h-full">
@@ -51,173 +47,18 @@ const Home: NextPage = () => {
           </div>
 
           <div
-            className="rounded-box"
+            className="AboutMe"
             style={{
               backgroundImage: "url(/AboutMe.png)",
-              backgroundSize: "contain", // Ajusta la imagen para contener todo el contenedor
-              backgroundPosition: "center", // Centra la imagen
-              backgroundRepeat: "no-repeat", // Evita que la imagen se repita
-              width: "100%", // Ajusta el ancho al 100%
-              height: "500px", // Ajusta la altura según sea necesario
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              width: "100%",
+              height: "500px",
             }}
           ></div>
 
-          <div className="carousel rounded-box w-64">
-            <div className="carousel-item w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-          </div>
-
-          <div className="carousel w-full">
-            <div id="item1" className="carousel-item w-full">
-              <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
-            </div>
-            <div id="item2" className="carousel-item w-full">
-              <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-            </div>
-            <div id="item3" className="carousel-item w-full">
-              <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-            </div>
-            <div id="item4" className="carousel-item w-full">
-              <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-            </div>
-          </div>
-          <div className="flex w-full justify-center gap-2 py-2">
-            <a href="#item1" className="btn btn-xs">
-              1
-            </a>
-            <a href="#item2" className="btn btn-xs">
-              2
-            </a>
-            <a href="#item3" className="btn btn-xs">
-              3
-            </a>
-            <a href="#item4" className="btn btn-xs">
-              4
-            </a>
-          </div>
-          <ul className="timeline timeline-vertical lg:timeline-horizontal">
-            <li>
-              <div className="timeline-start">1984</div>
-              <div className="timeline-middle">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="timeline-end timeline-box">First Macintosh computer</div>
-              <hr />
-            </li>
-            <li>
-              <hr />
-              <div className="timeline-start">1998</div>
-              <div className="timeline-middle">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="timeline-end timeline-box">iMac</div>
-              <hr />
-            </li>
-            <li>
-              <hr />
-              <div className="timeline-start">2001</div>
-              <div className="timeline-middle">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="timeline-end timeline-box">iPod</div>
-              <hr />
-            </li>
-            <li>
-              <hr />
-              <div className="timeline-start">2007</div>
-              <div className="timeline-middle">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="timeline-end timeline-box">iPhone</div>
-              <hr />
-            </li>
-            <li>
-              <hr />
-              <div className="timeline-start">2015</div>
-              <div className="timeline-middle">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="timeline-end timeline-box">Apple Watch</div>
-            </li>
-          </ul>
-          <div className="skeleton h-32 w-32"></div>
+          {/* Aquí se eliminó el carrusel */}
 
           {/* <p className="text-center text-lg">
             Get started by editing{" "}
